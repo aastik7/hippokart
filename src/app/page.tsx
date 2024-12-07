0fe74f05a0f37656c9f109b396d6a031367d777b
Aastik <aastikdude7@gmail.com>
2024-12-07 23:48:18 +0530

    fixed space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl ">
         <h1 className="text-4xl font-medium tracking-tight text-gray-900 sm:text-6xl ">
-          Your marketplace for high-quality
+          Your marketplace for high-quality{" "}
           <span className="text-blue-600">digital assets</span>
         </h1>
         <p className="mt-6 text-lg max-w-prose text-muted-foreground">
