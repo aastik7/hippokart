c5011d6d27c4806cc910202ab731af387de90d2b
Aastik <aastikdude7@gmail.com>
2024-12-31 00:25:33 +0530

    feat: added checkout button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,12 +4,15 @@ import { ShoppingCartIcon } from "lucide-react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
 import { Separator } from "@radix-ui/react-separator";
 import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
 
 const Cart = () => {
   const itemCount = 1;
@@ -52,6 +55,18 @@ const Cart = () => {
                   <span>{formatPrice(fee)}</span>
                 </div>
               </div>
+
+              <SheetFooter>
+                <SheetTrigger asChild>
+                  <Link
+                    href="/cart"
+                    className={buttonVariants({ className: "w-full" })}
+                  >
+                    {" "}
+                    Continue to Checkout
+                  </Link>
+                </SheetTrigger>
+              </SheetFooter>
             </div>
           </>
         ) : (
