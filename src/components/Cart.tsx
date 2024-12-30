3978e0f6534b6bac21123ef438f37cdda1026165
Aastik <aastikdude7@gmail.com>
2024-12-31 00:45:28 +0530

    feat: Added zero items hippo image

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,9 +13,10 @@ import { Separator } from "@radix-ui/react-separator";
 import { formatPrice } from "@/lib/utils";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 const Cart = () => {
-  const itemCount = 1;
+  const itemCount = 0;
 
   const fee = 1;
 
@@ -70,7 +71,15 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div></div>
+          <div className="flex h-full flex-col items-center justify-center space-y-1">
+            <div aria-hidden="true" className="relative mb-4 h-60 w-60 ">
+              <Image
+                src="/hippo-empty-cart.png"
+                alt="empty shopping cart hippo"
+                fill
+              />
+            </div>
+          </div>
         )}
       </SheetContent>
     </Sheet>
